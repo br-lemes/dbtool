3eca0b39750ae86fc48d121907efbbd045613f3d
Breno Ramalho Lemes <breno@br-lemes.net>
2025-08-03 22:32:59 -0400

    test: adicionar teste para script SQL inválido

--- src/Tests/RunCommandTest.php
+++ src/Tests/RunCommandTest.php
@@ -3,8 +3,18 @@ declare(strict_types=1);
 
 namespace DBTool\Tests;
 
+use Symfony\Component\Console\Command\Command;
+
 final class RunCommandTest extends AbstractCommandTestCase
 {
+    function testCommand(): void
+    {
+        $bad = __DIR__ . '/fixture/test-bad.sql';
+        $args = ['config' => 'test-mysql', 'script' => $bad];
+        $test = $this->exec('run', $args);
+        $this->assertEquals(Command::FAILURE, $test->getStatusCode());
+    }
+
     function testComplete(): void
     {
         $this->assertCompleteDatabase('run', ['']);
@@ -17,6 +27,7 @@ final class RunCommandTest extends AbstractCommandTestCase
             'run',
             ['test-mysql', 'src/Tests/fixture/'],
             [
+                'src/Tests/fixture/test-bad.sql',
                 'src/Tests/fixture/test-mariadb.sql',
                 'src/Tests/fixture/test-mysql.sql',
                 'src/Tests/fixture/test-pgsql.sql',
